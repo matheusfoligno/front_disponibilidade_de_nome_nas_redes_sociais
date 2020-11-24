46e9ca2bc99d1aa32e38ecb2504489a992a8d40f
Matheus Fernandes <matheusfoligno@gmail.com>
2020-11-23 21:49:12 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'front-nomedeusuariosdisponiveis';
+
+  testes = [
+    {id: 1, name:'Instagram'},
+    {id: 2, name:'Facebook'},
+    {id: 5, name:'Twiiter'}
+  ];
 }
